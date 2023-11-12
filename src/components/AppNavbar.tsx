f065b4c9e31c43d39a7ec7b8063826ae01e1becb
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-12 22:54:11 +0100

    Added the navbar component

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -2,7 +2,8 @@
 
 export default function AppNavbar() {
 
-    return(       
+    return(   
+        <>    
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -36,4 +37,8 @@ export default function AppNavbar() {
         </div>
         </div>
         </nav>
-    )}
+
+        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+        </>
+    );
+}
