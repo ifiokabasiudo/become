6dbbf28e5996043b6d8bdad4f08ba02d62b72aec
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-12 23:06:42 +0100

    Added the navbar component

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -9,9 +9,7 @@ export default function Scheduler() {
             <AppNavbar />
             <h1>Hello</h1>
 
-            
-
-            <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+            <script src="../../path/to/flowbite/dist/flowbite.min.js"></script>
         </div>
     )
 }
