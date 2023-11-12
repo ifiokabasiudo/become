c14f4b30525a4b7506ca76dacea7e55a2e0f4e31
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 00:06:43 +0100

    Added the script tag

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -7,6 +7,8 @@ export default function Scheduler() {
         <div>
             <AppNavbar />
             <h1>Hello</h1>
+
+            <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
         </div>
     )
 }
