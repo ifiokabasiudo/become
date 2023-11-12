91e186a22b63f6a1469f5f1f08a3aee0b2f7e691
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-12 23:58:56 +0100

    Added the navbar component

---
+++ app/scheduler/page.tsx
@@ -0,0 +1,12 @@
+import supabase from '../../src/components/supabase'
+import AppNavbar from '../../src/components/navbar'
+
+export default function Scheduler() {
+
+    return(
+        <div>
+            <AppNavbar />
+            <h1>Hello</h1>
+        </div>
+    )
+}
