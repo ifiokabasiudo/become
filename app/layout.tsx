07ce809655c17ce8a612fdb8ec60e8dd2e266d81
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-12 22:48:39 +0100

    Added the navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
       </body>      
     </html>
   )
