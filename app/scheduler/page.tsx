18e3e59aad248de61e9d63098e13f6efac900336
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 02:01:11 +0100

    Added the script tag

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -1,18 +1,14 @@
 import supabase from '../../src/components/supabase'
-import AppNavbar from '../../src/components/navbar'
+import AppNavbar from '../navbar'
 
 export default function Scheduler() {
 
     return(
         <div>
-            <div>  
-                <nav className= "fixed flex flex-row h-10 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-                    <div className="text-left">
-                        <h1 className="text-8xl">Scheduler</h1>
-                    </div>    
-                </nav>
+            <AppNavbar />
+            <div>
+                <h1>Hello</h1>
             </div>
-            <h1>Hello</h1>
         </div>
     )
 }
