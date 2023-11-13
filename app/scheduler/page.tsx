0cc1df75d597fd4b8aeab7dd5c54052135855ac5
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 20:10:41 +0100

    Added login and signup

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -1,5 +1,6 @@
 import supabase from '../../src/components/supabase'
 import AppNavbar from '../navbar'
+import Collection from './collections'
 
 export default function Scheduler() {
 
@@ -8,7 +9,11 @@ export default function Scheduler() {
             <AppNavbar />
             <div className='mt-28'>
                 <div className='border-b dark-nav-border-color flex flex-row text-left'>
-                    <h1 className='mb-12 ml-28 mr-20 text-4xl'>Let's keep our day orgainized</h1>
+                    <h1 className='mb-12 px-28 text-4xl'>Let's keep our day orgainized</h1>
+                </div>
+
+                <div id='collections' className='py-5 px-20'>
+                    <Collection />
                 </div>
             </div>
         </div>
