f921110c4c3449f0fd1d5568f5f392b61001a769
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 22:34:50 +0100

    Added the add collections button

--- app/scheduler/addCollections.tsx
+++ app/scheduler/addCollections.tsx
@@ -5,8 +5,8 @@ export default async function AddCollections() {
     }
 
     return (
-        <div className="py-5 px-20">
-            <button onClick={openModal} className="bg-transparent w-full hover:buttons text-white font-semibold hover:text-white border dark-nav-border-color rounded">
+        <div className="py-5 px-28">
+            <button onClick={openModal} className="py-2 bg-transparent w-full hover:buttons text-white font-semibold hover:text-white border dark-nav-border-color rounded">
                 Add Collection
             </button>
         </div>
--- app/scheduler/collections.tsx
+++ app/scheduler/collections.tsx
@@ -3,7 +3,7 @@
 export default function Collection() {
 
     return(
-        <div className="py-5 px-20">
+        <div className="py-5 px-28">
             <p>Collections go here</p>
         </div>
     )
