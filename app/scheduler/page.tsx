0d741e188928a511104bf481ac54c77033e64f57
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 01:58:05 +0100

    Added the script tag

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -5,7 +5,13 @@ export default function Scheduler() {
 
     return(
         <div>
-            <AppNavbar />
+            <div>  
+                <nav className= "fixed flex flex-row h-10 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+                    <div className="text-left">
+                        <h1 className="text-8xl">Scheduler</h1>
+                    </div>    
+                </nav>
+            </div>
             <h1>Hello</h1>
         </div>
     )
