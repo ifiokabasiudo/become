45a090889c3a521f883769f91d61394dfb8e8b91
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 18:49:29 +0100

    Added lets keep our day organized

--- app/scheduler/page.tsx
+++ app/scheduler/page.tsx
@@ -6,8 +6,8 @@ export default function Scheduler() {
     return(
         <div>
             <AppNavbar />
-            <div>
-                <h1 className='mt-24'>Hello</h1>
+            <div className='border-b dark-nav-border-color flex flex-row text-left'>
+                <h1 className='mt-32 mb-24 ml-6 text-4xl'>Let's keep our day orgainized</h1>
             </div>
         </div>
     )
