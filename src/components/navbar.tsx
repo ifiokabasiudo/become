d45ebbc0d75701fbfed9d7994f5410c6f1e6322c
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 01:52:56 +0100

    Added the script tag

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,13 @@
 
 export default function AppNavbar() {
 
-    return(   
-        <nav className="bg-white dark:bg-gray-900 fixed flex flex-row h-10 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        <div className="text-left">
-            <h1 className="text-4xl">Scheduler</h1>
-        </div>    
-        </nav>
+    return( 
+        <div>  
+            <nav className="bg-white dark:bg-gray-900 fixed flex flex-row h-10 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+                <div className="text-left">
+                    <h1 className="text-8xl">Scheduler</h1>
+                </div>    
+            </nav>
+        </div>
     )
 }
