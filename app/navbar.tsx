e5af48e0b8ae05064a903fc48fbe774440800ba3
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 02:15:16 +0100

    Changed the margin and size the navbar scheduler

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,7 +6,7 @@ export default function AppNavbar() {
         <div>  
             <nav className= "fixed flex flex-row w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
                 <div className="text-left">
-                    <h1 className="text-4xl m-3">Scheduler</h1>
+                    <h1 className="text-3xl m-6">Scheduler</h1>
                 </div>    
             </nav>
         </div>
