65050e5856d88e3a980b70ecee89b7212b586817
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-14 00:03:55 +0100

    Added a white sidebar

--- app/scheduler/scheduler.tsx
+++ app/scheduler/scheduler.tsx
@@ -3,9 +3,16 @@
 import supabase from '../../src/components/supabase'
 import AppNavbar from '../navbar'
 import Collection from './collections'
-import AddCollections from './addCollections'
+// import AddCollections from './addCollections'
+import { useState } from 'react'
 
 export default async function Scheduler( {session}: any ) {
+    const [openSidebar, setOpenSidebar] = useState(false)
+
+    const openModal = () => {
+        console.log("The modal was opened")
+        setOpenSidebar(true)
+    }
 
     let username
 
@@ -17,12 +24,23 @@ export default async function Scheduler( {session}: any ) {
     return(
         <div>
             <AppNavbar username = {username}/>
-            <div className='mt-28'>
+            <div className='mt-28 relative'>
+                {
+                    openSidebar && (
+                        <div className='absolute right-0 top-0 h-[100vh] w-[40%] bg-white z-10'>
+
+                        </div>
+                    )
+                }
                 <div className='border-b dark-nav-border-color flex flex-row text-left'>
                     <h1 className='mb-12 px-28 text-4xl'>Let's keep our day orgainized</h1>
                 </div>
 
-                <AddCollections />
+                <div className="py-5 px-28">
+                    <button onClick={openModal} className="py-2 bg-transparent w-full hover:buttons text-white font-semibold hover:text-white border dark-nav-border-color rounded">
+                        Add Collection
+                    </button>
+                </div>
 
                 <div id='collections'>
                     <Collection />
