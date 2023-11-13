a815c9857893ca4d5c70f57b1226388245a3d053
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-13 02:08:10 +0100

    Added the script tag

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -6,7 +6,7 @@ export default function AppNavbar() {
         <div>  
             <nav className= "fixed flex flex-row h-10 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
                 <div className="text-left">
-                    <h1 className="text-8xl">Scheduler</h1>
+                    <h1 className="text-4xl mx-3">Scheduler</h1>
                 </div>    
             </nav>
         </div>
