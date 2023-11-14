728a4d67b6f6f0a0de61c319c070953de23c36a8
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-14 01:34:53 +0100

    Trying to troubleshoot errors

--- app/scheduler/scheduler.tsx
+++ app/scheduler/scheduler.tsx
@@ -4,15 +4,15 @@ import supabase from '../../src/components/supabase'
 import AppNavbar from '../navbar'
 import Collection from './collections'
 // import AddCollections from './addCollections'
-import { useState } from 'react'
+// import { useState } from 'react'
 
 export default async function Scheduler( {session}: any ) {
-    const [openSidebar, setOpenSidebar] = useState(false)
+    // const [openSidebar, setOpenSidebar] = useState(false)
 
-    const openModal = () => {
-        console.log("The modal was opened")
-        setOpenSidebar(true)
-    }
+    // const openModal = () => {
+    //     console.log("The modal was opened")
+    //     setOpenSidebar(true)
+    // }
 
     let username
 
@@ -25,19 +25,19 @@ export default async function Scheduler( {session}: any ) {
         <div>
             <AppNavbar username = {username}/>
             <div className='mt-28 relative'>
-                {
-                    openSidebar && (
+                {/* {
+                    openSidebar && ( */}
                         <div className='absolute right-0 top-0 h-[100vh] w-[40%] bg-white z-10'>
-
+                            <p>Just thought I should add something in here</p> 
                         </div>
-                    )
-                }
+                    {/* )
+                } */}
                 <div className='border-b dark-nav-border-color flex flex-row text-left'>
                     <h1 className='mb-12 px-28 text-4xl'>Let's keep our day orgainized</h1>
                 </div>
 
                 <div className="py-5 px-28">
-                    <button onClick={openModal} className="py-2 bg-transparent w-full hover:buttons text-white font-semibold hover:text-white border dark-nav-border-color rounded">
+                    <button className="py-2 bg-transparent w-full hover:buttons text-white font-semibold hover:text-white border dark-nav-border-color rounded">
                         Add Collection
                     </button>
                 </div>
