34f2072c36ccbc2a07ce4c3036a7a0cd424e6b93
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2023-11-29 21:18:36 +0100

    Fixed the signup

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,12 +8,14 @@ export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
+  console.log(request.url)
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
 
   if (code) {
     const supabase = createRouteHandlerClient({ cookies })
     await supabase.auth.exchangeCodeForSession(code)
+    return NextResponse.redirect("https://become-one.vercel.app/scheduler")
   }
 
   // URL to redirect to after sign in process completes
